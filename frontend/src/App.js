915b985fd2082272572208c510846dffd0de10a0
moonmusiclibrary02-hue <moonmusiclibrary02@gmail.com>
2025-10-17 19:02:45 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ import ManagerProfile from './components/ManagerProfile';
 import AdminSettings from './components/AdminSettings';
 import './App.css';
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = window.BACKEND_URL || 'http://localhost:8000';
 const API = `${BACKEND_URL}/api`;
 
 // Create axios instance with interceptor for auth
